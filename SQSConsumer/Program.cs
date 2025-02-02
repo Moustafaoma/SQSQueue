10697d130108d6251c75403f45793bf9e7e3c99c
Moustafa Omar <moustafa.omar@tkmind.net>
2025-02-02 09:55:51 +0200

    Add project files.

---
+++ SQSConsumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SQSConsumer;
+using System.Text.Json;
+
+namespace SQSConsumer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cts=new CancellationTokenSource();
+            var sqsClient = new AmazonSQSClient();
+            var queueUrlResponse = await sqsClient.GetQueueUrlAsync("Customers");
+            var recieveMessageRequest = new ReceiveMessageRequest()
+            {
+                QueueUrl = queueUrlResponse.QueueUrl,
+
+            };
+
+            while (!cts.IsCancellationRequested)
+            {
+                var response = await sqsClient.ReceiveMessageAsync(recieveMessageRequest,cts.Token);
+                foreach (var message in response.Messages) {
+                    Console.WriteLine(message.MessageId);
+                    Console.WriteLine(message.Body);
+                    await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl,message.ReceiptHandle);
+                }
+                await Task.Delay(3000);
+            }
+        }
+    }
+}
---
+++ SQSQueue/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SQSQueue;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+
+public class Program
+{
+    static async Task Main(string[] args) 
+    {
+        var sqsClient = new AmazonSQSClient(); 
+
+        var customer = new CustomerData
+        {
+            Id = 8,
+            Email = "mohamed.omar@example.com",
+            Name = "Mohamed",
+            CreatedAt = DateTime.UtcNow
+        };
+
+        var queueUrlClient = await sqsClient.GetQueueUrlAsync("Customers"); 
+
+        var sendMessageRequest = new SendMessageRequest()
+        {
+            QueueUrl = queueUrlClient.QueueUrl, 
+            MessageBody = JsonSerializer.Serialize(customer), 
+        };
+
+        var response = await sqsClient.SendMessageAsync(sendMessageRequest); 
+
+        Console.WriteLine($"✅ Message Sent! MessageId: {response.MessageId}");
+    }
+}
+
